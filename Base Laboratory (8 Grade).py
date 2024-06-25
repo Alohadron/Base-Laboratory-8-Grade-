10e7ca0bc730a540396ea115ea6a9fec1eff3617
Alohadron <alohadron2012@gmail.com>
2024-06-25 13:51:05 +0300

    First Push

---
+++ Base Laboratory (8 Grade).py
@@ -0,0 +1,145 @@
+from datetime import date
+
+# menu ="""---------------------------------------------------------------
+# Welcome to TUM's student management system!
+# What do you want to do?
+# g - General operations
+# f - Faculty operations
+# s - Student operations
+
+# q - Quit Program"""
+
+# general_operations = """---------------------------------------------------------------
+# General operations
+# What do you want to do?
+
+# nf/<faculty name>/<faculty abbreviation>/<field> - create faculty
+# ss/<student email> - search student and show faculty
+# df - display faculties
+# df/<field> - display all faculties of a field
+
+# b - back
+# q - Quit Program
+# """
+
+# faculty_operations = """---------------------------------------------------------------
+# Faculty operations
+# What do you want to do?
+
+# ns - create student
+# gs - graduate student
+# ds/<faculty abbreviation> - display enrolled students
+# dg/<faculty abbreviation> - display graduated students
+# bf/<faculty abbreviation>/<email> - check if student belongs to faculty
+
+# b - back
+# q - Quit Program
+# """
+
+# student_operation = """---------------------------------------------------------------
+# Student operations
+# What do you want to do?
+
+# There is nothing to do here at the moment.
+
+# b - back
+# q - Quit Program
+# """
+
+
+#------------------------------------------------------------------------------------------------------
+#Creaza un Student
+# class Student:
+#     def __init__(self) -> None:
+#         self.facultyAbbreviation,self.firstName,self.lastName,self.email,self.dateOfBirth = input().split("/")
+        
+#     def studC(self):
+#         print("Succes!\nFaculty Abbreviation:", self.facultyAbbreviation,
+#                "\nName:", self.firstName, self.lastName,
+#                "\nEmail: ",self.email,
+#                "\nDate of Birth: ",self.dateOfBirth)
+        
+#         with open("ToGitHub\StudentList.txt","a") as f:
+#             f.write(f"{self.facultyAbbreviation},")
+#             f.write(f"{self.firstName},")
+#             f.write(f"{self.lastName},")
+#             f.write(f"{self.email},")
+#             f.write(f"{self.dateOfBirth}\n")
+        
+# def createStudent():
+#     print("Creaza un Student.\nExemplu:<faculty abbreviation>/<first name>/<last name>/<email>/<dateOfBirth")
+#     stud = Student()
+#     stud.studC()
+
+#------------------------------------------------------------------------------------------------------
+#Graduate a student
+with open("ToGitHub\StudentList.txt","r") as f:
+    data = f.readlines()
+    student_data = []
+    for line in data:
+        noN = line.replace("\n","")
+        splited = noN.split(",")
+        student_data.append(splited)
+print(student_data)
+def grad_stud(): 
+    x = input()
+    for elem in student_data:
+        if elem[3] == x:
+           print(f"Congratulations, {elem[1]} has graduated")
+           with open("ToGitHub\Graduated.txt","a") as f:               
+               for item in elem:
+                   f.write(item + ",") 
+               f.write("\n")
+           
+        
+            
+                  
+#------------------------------------------------------------------------------------------------------
+# Functia Terminal
+# def terminal_menu():
+#     print(menu)
+#     inp1 = input()
+#     if inp1 == "g":
+#         print(general_operations)
+#         inp2 = input()
+#         if inp2 == "b":
+#             terminal_menu()
+#         elif inp2 == "q":
+#             print("Program quited")
+#             False
+#     elif inp1 == "f":
+#         print(faculty_operations)
+#         inp2 = input()
+#         if inp2 == "ns":
+#             createStudent()
+#             terminal_menu()
+#         elif inp2 == "gs":
+#             print("Enter student's email for graduation:")
+#             grad_stud()
+#             terminal_menu()        
+#         elif inp2 == "b":
+#             terminal_menu()
+#         elif inp2 == "q":
+#             print("Program quited")
+#             False
+#     elif inp1 == "s":
+#         print(student_operation)
+#         inp2 = input()
+#         if inp2 == "b":
+#             terminal_menu()
+#         elif inp2 == "q":
+#             print("Program quited")
+#             False
+#     elif inp1 == "q":
+#         print("Program quited")
+#         False
+   
+# terminal_menu()
+
+
+
+
+
+
+
+
